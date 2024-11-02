e69bb75ee993770fbcb3bf4cb00f928efdf9c88c
Ytalo Santos <ytalos762@gmail.com>
2024-11-02 17:49:36 -0300

    Spring Boot Project

---
+++ src/main/java/com/project/apirestjava/ApirestjavaApplication.java
@@ -0,0 +1,13 @@
+package com.project.apirestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/apirestjava/ApirestjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apirestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
